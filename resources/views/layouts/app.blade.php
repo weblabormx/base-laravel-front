ed2769dba6736e5682ab2fde55a67aede26611ff
Carlos Escobar <carlos@xmlteam.com>
2021-06-17 15:58:22 -0500

    Fix error

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,10 +12,6 @@
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
 
-    <!-- Fonts -->
-    <link rel="dns-prefetch" href="//fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
-
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     <link rel="stylesheet" type="text/css" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
@@ -106,6 +102,6 @@
         </main>
     </div>
 
-    @yield('scripts-footer')
+    @stack('scripts-footer')
 </body>
 </html>
