ce001ba39b5aa96199cf0da41c2a25e0cf502a81
Carlos Escobar <carlosescobar@Mac-mini-de-Carlos.local>
2021-06-18 01:19:02 -0500

    Update

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -101,7 +101,10 @@
             @endif
         </main>
     </div>
-
+    <script
+    src="https://code.jquery.com/jquery-3.4.1.min.js"
+    integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo="
+    crossorigin="anonymous"></script>
     @stack('scripts-footer')
 </body>
 </html>
