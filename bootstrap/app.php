aa9552756a923781b96f546ebc62e600e14a6e78
Luan Himmlisch <luan@himmlisch.com.mx>
2024-09-06 19:44:17 -0600

    Defined routes as Laravel 11

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,11 +3,23 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Support\Facades\Route;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        commands: __DIR__ . '/../routes/console.php',
+        commands: base_path('routes/console.php'),
         health: '/up',
+        using: function () {
+            Route::prefix('api')
+                ->middleware('api')
+                ->group(base_path('routes/api.php'));
+
+            Route::middleware('web')
+                ->group(base_path('routes/auth.php'));
+
+            Route::middleware('web')
+                ->group(base_path('routes/web.php'));
+        }
     )
     ->withMiddleware(function (Middleware $middleware) {
         $middleware->redirectTo(
--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -1,7 +1,7 @@
 <?php
 
 return [
-    App\Providers\RouteServiceProvider::class,
-    App\Providers\AppServiceProvider::class,
+    App\Providers\RateLimiterProvider::class,
     App\Providers\LocalTemporaryUrlProvider::class,
+    App\Providers\AppServiceProvider::class,
 ];
