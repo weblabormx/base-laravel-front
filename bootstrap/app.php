6361fe931e1734ec7b8b7f923e19528ab50b6af2
Luan Himmlisch <luan@himmlisch.com.mx>
2024-09-06 19:27:31 -0600

    Fixed redirects

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -10,7 +10,10 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        //
+        $middleware->redirectTo(
+            guests: '/login',
+            users: '/app',
+        );
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
