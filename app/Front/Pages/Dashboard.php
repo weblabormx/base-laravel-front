79bf2a7a9708be4029955f9ce558331b880fe05b
Luan Himmlisch <luan@himmlisch.com.mx>
2024-09-07 00:39:12 -0600

    Finished Dashboard as example page

--- app/Front/Pages/Dashboard.php
+++ app/Front/Pages/Dashboard.php
@@ -2,9 +2,10 @@
 
 namespace App\Front\Pages;
 
-use App\Front\Inputs\WireInput;
+use App\Front\Inputs as Custom;
 use Spatie\Html\Elements as Html;
-use WeblaborMx\Front\Components\Welcome;
+use WeblaborMx\Front\Components;
+use WeblaborMx\Front\Inputs;
 
 class Dashboard extends Page
 {
@@ -13,25 +14,36 @@ class Dashboard extends Page
 
 
         return [
-            Welcome::make(),
+            // Place here the Laravel Front field inputs and components 
+            Components\Welcome::make(),
 
+            Inputs\Text::make()
+                ->setValue('Your email is ' . auth()->user()->email)
+                ->addAttribute('readonly', true),
+
+            // You can also place any object that implements `Htmlable`
             Html\Div::create()->class('border-b-2 my-7'),
 
-            WireInput::make('modal')
+            // You can create your own custom Inputs
+            // This one can render WireUI components
+            Custom\WireInput::make('modal')
                 ->addAttribute('name', 'my-modal')
                 ->addAttribute('blurness', true)
                 ->children([
-                    WireInput::make('card')
-                        ->addAttribute('title', 'Wenas')
+                    Custom\WireInput::make('card')
+                        ->addAttribute('title', 'Meow!')
                         ->class('w-full')
                         ->children([
                             Html\Img::create()
-                                ->src('//placekitten.com/1000/1000')
+                                ->src('//placekitten.com/1000/1000'),
+                            Html\P::create()
+                                ->text('Have a nice day...')
+                                ->class('text-center text-gray-700 mt-5 italic')
                         ])
                 ]),
 
-            WireInput::make('button', [
-                'label' => 'Abre el modal',
+            Custom\WireInput::make('button', [
+                'label' => 'Click me :D',
                 'x-on:click' => "\$openModal('my-modal')",
                 'full' => true
             ]),
