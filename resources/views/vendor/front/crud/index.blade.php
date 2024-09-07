30d53695a68cf88af6036d4daa262ff281bac938
Luan Himmlisch <luan@himmlisch.com.mx>
2024-09-06 19:27:19 -0600

    Updated published front views

--- resources/views/vendor/front/crud/index.blade.php
+++ resources/views/vendor/front/crud/index.blade.php
@@ -2,17 +2,17 @@
 
 @section('sidebar')
 
-    @if(count($front->filters())>0)
+    @if (count($front->filters()) > 0)
         <div class="mt-6 font-bold">{{ __('FILTER :name', ['name' => strtoupper($front->plural_label)]) }}</div>
-        {!! Form::open(['url' => request()->url(), 'method' => 'get']) !!} 
-            <div class="card pt-3 sidenav-forms">
-                {!! Form::hidden($front->getCurrentViewRequestName()) !!}
-                @foreach($front->getFilters() as $filter)
-                    {!! $filter->formHtml() !!}
-                @endforeach
-            </div>
-            {!! Form::submit(__('Search'), ['class' => 'bg-green-800 text-white py-2 px-4 rounded block w-full mt-2']) !!}
-        {!! Form::close() !!}
+        {{ html()->form('GET', request()->url())->open() }}
+        <div class="pt-3 card sidenav-forms">
+            {!! Form::hidden($front->getCurrentViewRequestName()) !!}
+            @foreach ($front->getFilters() as $filter)
+                {!! $filter->formHtml() !!}
+            @endforeach
+        </div>
+        {{ html()->submit(__('Search'))->class('bg-green-800 text-white py-2 px-4 rounded block w-full mt-2') }}
+        {{ html()->form()->close() }}
     @endif
 
 @endsection
@@ -23,20 +23,20 @@
     @include('front::elements.breadcrumbs')
 
     <div class="mt-2 md:flex md:items-center md:justify-between">
-        <div class="min-w-0 flex-1">
-            <h2 class="text-2xl font-bold leading-7 text-gray-900 sm:truncate sm:text-3xl sm:tracking-tight">{{$front->plural_label}}</h2>
+        <div class="flex-1 min-w-0">
+            <h2 class="text-2xl font-bold leading-7 text-gray-900 sm:truncate sm:text-3xl sm:tracking-tight">{{ $front->plural_label }}</h2>
         </div>
-        <div class="mt-4 flex flex-shrink-0 md:mt-0 md:ml-4">
-            @foreach($front->getIndexLinks() as $button)
+        <div class="flex flex-shrink-0 mt-4 md:mt-0 md:ml-4">
+            @foreach ($front->getIndexLinks() as $button)
                 {!! $button->form() !!}
             @endforeach
         </div>
     </div>
 
-    @if($front->getLenses()->count() > 1)
+    @if ($front->getLenses()->count() > 1)
         <div>
             <h4>Lenses</h4>
-            @foreach($front->getLenses() as $button)
+            @foreach ($front->getLenses() as $button)
                 {!! $button->form() !!}
             @endforeach
         </div>
@@ -46,4 +46,3 @@
     @include ($front->getCurrentView())
 
 @endsection
-
--- resources/views/vendor/front/crud/partial-create.blade.php
+++ resources/views/vendor/front/crud/partial-create.blade.php
@@ -1,17 +1,17 @@
-{!! Form::open(array('url' => $front->getBaseUrl(), 'files' => true)) !!}
+{{ html()->form('POST', $front->getBaseUrl())->acceptsFiles()->open() }}
 
-    {!! Form::hidden('redirect_url') !!}
-    @foreach($front->createPanels() as $panel)
-        {!! $panel->formHtml() !!}
-    @endforeach
-    <div class="text-right mt-3">
-        <button type="submit" class="ml-3 inline-flex items-center rounded-md border border-transparent bg-blue-600 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2">
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 mr-2">
-              <path stroke-linecap="round" stroke-linejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
-            </svg>
+{{ html()->hidden('redirect_url') }}
+@foreach ($front->createPanels() as $panel)
+    {!! $panel->formHtml() !!}
+@endforeach
+<div class="mt-3 text-right">
+    <button type="submit" class="inline-flex items-center px-4 py-2 ml-3 text-sm font-medium text-white bg-blue-600 rounded-md border border-transparent shadow-sm hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2">
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="mr-2 w-6 h-6">
+            <path stroke-linecap="round" stroke-linejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
+        </svg>
 
-            {{ __('Add') }} {{$front->label}}
-        </button>
-    </div>
+        {{ __('Add') }} {{ $front->label }}
+    </button>
+</div>
 
-{!! Form::close() !!}
+{{ html()->form()->close() }}
